ac7d2de1aebfa99c10c13126283ee97ccdb59bbf
Maria Aguilar <maria.aguilar.c@tecsup.edu.pe>
2021-04-15 21:31:43 -0500

    segundo commit

---
+++ directorio/src/App.js
@@ -0,0 +1,65 @@
+import React, { useState } from 'react'
+
+import './App.css';
+
+const App = () => {
+
+  const [ persons, setPersons ] = useState(
+    [
+      { 
+        name: 'Arto Hellas' 
+      }
+    ]
+  ) 
+
+  const [ newName, setNewName ] = useState('')
+
+  const handleChange = (event) =>{
+    const nuevoNombre = event.target.value
+    setNewName(nuevoNombre)
+  }
+
+  const handleClick = (e)=>{
+    e.preventDefault();
+    const newPerson = {
+      name: newName
+    }
+
+    setPersons([
+      ...persons,
+      newPerson
+    ])
+
+    setNewName('')
+    
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+
+      <form>
+
+        <div>
+          name: 
+          <input value={newName} type="text" onChange={handleChange}/>
+        </div>
+
+        <div>
+          <button type="submit" onClick={handleClick}>add</button>
+        </div>
+
+      </form>
+
+      <h2>Numbers</h2>
+      {
+        persons.map((person) =>(
+          <p key={person.name}>{person.name}</p>
+        ))
+      }
+
+    </div>
+  )
+}
+
+export default App;
